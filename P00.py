3a8b2f0524cf1034c065ed7d465afb1d6c8a19c7
Kai Wang <charles.wangkai@gmail.com>
2015-05-06 11:06:12 +0800

    Level 0

---
+++ P00.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Q: http://www.pythonchallenge.com/pc/def/0.html
+# A: http://www.pythonchallenge.com/pc/def/274877906944.html
+
+def main():
+    print(2 ** 38)
+
+if __name__ == '__main__':
+    main()
