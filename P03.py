cc5404825ac989103bc730518e97d46e22738313
Kai Wang <charles.wangkai@gmail.com>
2015-05-07 21:41:16 +0800

    Level 3

---
+++ P03.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Q: http://www.pythonchallenge.com/pc/def/equality.html
+# A: http://www.pythonchallenge.com/pc/def/linkedlist.php
+
+import re
+import urllib
+
+def main():
+    source = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/equality.html').read().decode()
+    mess = re.findall(r'(?<=<!--).*?(?=-->)', source, flags=re.DOTALL)[0].replace('\n', '')
+    print(''.join(re.findall(r'(?<=[^A-Z][A-Z]{3})[a-z](?=[A-Z]{3}[^A-Z])', mess)))
+
+if __name__ == '__main__':
+    main()
