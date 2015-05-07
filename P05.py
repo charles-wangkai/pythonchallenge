45ccd7496310d1dbd491658cfc26f83a9f74cad3
Kai Wang <charles.wangkai@gmail.com>
2015-05-08 06:10:43 +0800

    Level 5

---
+++ P05.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Q: http://www.pythonchallenge.com/pc/def/peak.html
+# A: http://www.pythonchallenge.com/pc/def/channel.html
+
+import pickle
+import urllib
+
+def main():
+    f = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/banner.p')
+    lines = pickle.load(f)
+    for line in lines:
+        print(''.join([elem[0] * elem[1] for elem in line]))
+
+if __name__ == '__main__':
+    main()
