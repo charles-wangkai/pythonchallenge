1151db7013fdda2383aeff417d615ab1a84e8a91
Kai Wang <charles.wangkai@gmail.com>
2015-05-08 05:42:10 +0800

    Level 4

---
+++ P04.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Q: http://www.pythonchallenge.com/pc/def/linkedlist.php
+# A: http://www.pythonchallenge.com/pc/def/peak.html
+
+import re
+import urllib
+
+def main():
+    nothing = 12345
+    while True:
+        source = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={nothing}'.format(nothing=nothing)).read().decode()
+        print(source)
+        
+        match = re.search(r'the next nothing is (\d+)', source)
+        if match:
+            nothing = int(match.group(1))
+        elif 'Divide by two' in source:
+            nothing /= 2
+        else:
+            break
+
+if __name__ == '__main__':
+    main()
