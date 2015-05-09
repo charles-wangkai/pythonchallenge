ba4c7e1bb2718da8e86184d6abfb4b254e6dfd38
Kai Wang <charles.wangkai@gmail.com>
2015-05-09 16:06:38 +0800

    Level 8

---
+++ P08.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Q: http://www.pythonchallenge.com/pc/def/integrity.html
+# A: http://www.pythonchallenge.com/pc/return/good.html
+
+import ast
+import bz2
+import re
+import urllib
+
+def main():
+    source = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/integrity.html').read().decode()
+    un = re.search(r'un: \'(.+)\'', source).group(1)
+    pw = re.search(r'pw: \'(.+)\'', source).group(1)
+    
+    username = bz2.decompress(ast.literal_eval('b"{un}"'.format(un=un))).decode()
+    password = bz2.decompress(ast.literal_eval('b"{pw}"'.format(pw=pw))).decode()
+    print('username:', username)
+    print('password:', password)
+
+if __name__ == '__main__':
+    main()
