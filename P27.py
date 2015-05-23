a721e04452793db60ebe62b93cd4d93a80fcec74
Kai Wang <charles.wangkai@gmail.com>
2015-05-23 12:15:39 +0800

    Level 27

---
+++ P27.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Q: http://www.pythonchallenge.com/pc/hex/speedboat.html
+# A: http://www.pythonchallenge.com/pc/ring/bell.html (username: repeat / password: switch)
+
+import builtins
+import bz2
+import keyword
+import urllib.request
+from PIL import Image
+
+def configure_auth():
+    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, 'http://www.pythonchallenge.com', 'butter', 'fly')
+    auth_handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+    opener = urllib.request.build_opener(auth_handler)
+    urllib.request.install_opener(opener)
+
+def is_reserved_word(word):
+    return word in keyword.kwlist or word in dir(builtins)
+
+def main():
+    configure_auth()
+    local_filename = urllib.request.urlretrieve('http://www.pythonchallenge.com/pc/hex/zigzag.gif')[0]
+    image = Image.open(local_filename)
+    
+    original_content = ''.join(map(chr, image.getdata()))
+    
+    translated_content = original_content.translate(str.maketrans(''.join([chr(i) for i in range(256)]), ''.join(map(chr, image.getpalette()[::3]))))
+    
+    content_pairs = list(zip(original_content[1:], translated_content[:-1]))
+    
+    new_im = Image.new('1', image.size)
+    new_im.putdata([pair[0] == pair[1] for pair in content_pairs])
+    new_im.show()
+    
+    diffs = list(filter(lambda pair: pair[0] != pair[1], content_pairs))
+    words = bz2.decompress(''.join(list(zip(*diffs))[0]).encode('latin-1')).decode().split()
+    
+    for word in set(words):
+        if not is_reserved_word(word):
+            print(word)
+
+if __name__ == '__main__':
+    main()
