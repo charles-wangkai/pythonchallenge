67a51c4e08753fdc67657914b8b12a0b1f7bd5de
Kai Wang <charles.wangkai@gmail.com>
2015-05-25 13:31:44 +0800

    Level 29 [Move configure_auth() to common util]

--- P29.py
+++ P29.py
@@ -5,16 +5,10 @@
 
 import bz2
 import urllib.request
-
-def configure_auth():
-    password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-    password_mgr.add_password(None, 'http://www.pythonchallenge.com', 'repeat', 'switch')
-    auth_handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
-    opener = urllib.request.build_opener(auth_handler)
-    urllib.request.install_opener(opener)
+import PC_Util
 
 def main():
-    configure_auth()
+    PC_Util.configure_auth()
     source = urllib.request.urlopen('http://www.pythonchallenge.com/pc/ring/guido.html').read().decode()
     
     print(bz2.decompress(''.join(map(chr, map(len, filter(lambda line: not line.strip(), source.split('\n'))))).encode('latin-1')).decode())
